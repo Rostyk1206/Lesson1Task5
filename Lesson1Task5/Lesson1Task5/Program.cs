815bd09524a52321ab73ce97528bda55a0cdcb85
unknown <rostykvoznyak@gmail.com>
2020-10-22 21:01:32 +0300

    Lesson1Task5

---
+++ Lesson1Task5/Lesson1Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson1Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string surname = "Voznyak";
+            string name = "Rostyk";
+            string lastname = "Volodymyrovych";
+            Console.WriteLine($"First Name:{name}, Last Name:{lastname}, Surname:{surname}");
+        }
+    }
+}
